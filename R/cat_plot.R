ac6084866458f2f8c10333c50f0b73caf0a04cf5
David L Gibbs <gibbsdavidl@gmail.com>
2016-03-13 16:37:31 -0700

    plot update

--- R/cat_plot.R
+++ R/cat_plot.R
@@ -1,21 +1,13 @@
 
 
-#utility function for embedding png images at specified fractional sizes in R plots
-#places the logo centred on a specified fraction of the the usr space,
-#and sizes appropriately (respects aspect ratio)
-logoing_func<-function(logo, x, y, size){
-  dims<-dim(logo)[1:2] #number of x-y pixels for the logo (aspect ratio)
-  AR<-dims[1]/dims[2]
-  par(usr=c(0, 1, 0, 1))
-  rasterImage(logo, x-(size/2), y-(AR*size/2), x+(size/2), y+(AR*size/2), interpolate=TRUE)
-}
+# CatterPlot!
 
-
-catplot <- function(xs, ys, size, catmode, title) {
+catplot <- function(xs, ys, size=0.1, catmode=1, title="") {
 	require(png)
 	data(cats)
 	plot(x=xs, y=ys, col=0)
-	sapply(1:length(xs), function(i) logoing_func(catdat[[catmode]], x=xs[i]/max(xs), y=ys[i]/max(ys), size=size))
+	img <- catdat[[catmode]]
+	rasterImage(img, x-(size/2), y-(AR*size/2), x+(size/2), y+(AR*size/2), interpolate=TRUE)
 	title(main=title)
 }
 
