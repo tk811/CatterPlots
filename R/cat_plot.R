25a3b15537585d773277fc566073779451227ca2
David L Gibbs <gibbsdavidl@gmail.com>
2016-03-13 19:05:46 -0700

    different axis scales work now

--- R/cat_plot.R
+++ R/cat_plot.R
@@ -1,14 +1,27 @@
 
 
-# CatterPlot!
+# CatterPlot
 
 catplot <- function(xs, ys, size=0.1, catmode=1, title="") {
 	require(png)
 	data(cats)
-	plot(x=xs, y=ys, col=0)
-	img <- catdat[[catmode]]
-	rasterImage(img, x-(size/2), y-(AR*size/2), x+(size/2), y+(AR*size/2), interpolate=TRUE)
+
+	plot(x=xs, y=ys, col=0, xaxt="n", yaxt="n")
 	title(main=title)
+	par(usr=c(0,1,0,1))
+	axis(side=1, at=seq(0.1,1.0,0.1), labels=xs)
+	axis(side=2, at=seq(0.1,1.0,0.1), labels=ys)
+
+	img <- catdat[[catmode]]
+	dims<-dim(img)[1:2] #number of x-y pixels for the img (aspect ratio)
+  AR<-dims[1]/dims[2]
+
+	xscale <- xs + (-min(c(0,xs)))
+	yscale <- ys + (-min(c(0,ys)))
+	xscale <- xscale/max(xscale)
+	yscale <- yscale/max(yscale)
+
+	rasterImage(img, xscale-(size/2), yscale-(size/2), xscale+(size/2), yscale+(size/2), interpolate=TRUE)
 }
 
 # catplot(xs=abs(rnorm(10)), ys=abs(rnorm(10)), size=0.15, catmode=1, title="Cat Plot 1")
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Did you ever wish you could make scatter plots with cat shaped points?  Now you 
 
 Go ahead, treat yourself! There's over 10 cat models now!
 
-git clone the repo and install using: 
+git clone the repo and install using:
 
 ```
 R CMD INSTALL CatterPlot
@@ -13,7 +13,11 @@ In R:
 
 ```
 library(CatterPlot)
-catplot(xs=1:10, ys=1:10, size=1, catmode=1, title="My Cat Plot")
+x <- 1:10
+y <- 1:10
+catplot(x,y,0.1)
+catplot(x,y^2,0.1)
+catplot(x^2,y,0.1)
 ```
 
 NEW cat modes coming soon!
